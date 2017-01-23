35f0aee1dda32e2d3adfb3cd70bf1d9bc4713b34
Nicstradamus <nicholashardy@gmail.com>
2017-01-23 13:37:19 -0800

    Updated each doc import order and scripts

--- item65C_func.py
+++ item65C_func.py
@@ -5,8 +5,35 @@ import os
 from tkinter import filedialog
 from tkinter import messagebox
 
-
 import item65C_main
 import item65C_gui
 
-#def create_vars(src_files='', dest_path='', count=0, frame=''):
+
+
+def get_src(src_files,dest_path,count,frame):
+    src_files = filedialog.askopenfilenames()
+    count = int(0)
+    for i in src_files:
+        count += 1
+    lbl_count = ttk.Label(frame, text= 'Files selected: {}'.format(str(count))).pack()
+    print(src_files)
+    print(count)
+
+    
+
+def get_dest(src_files,dest_path,count,frame):
+    dest_path = filedialog.askdirectory()
+    lbl_dest = ttk.Label(frame, text='Destination folder: {}'.format(dest_path)).pack()
+
+    
+
+def move_files(src_files,dest_path,count,frame):
+    if count > 1 and len(dest_path) != '':
+        shutil.move(src_files,dest_path)
+    else:
+        messagebox.showinfo(title='Alert', message = 'You must first select files and a destination path.')
+        print(count)
+
+
+if __name__ == "__main__":
+    pass
--- item65C_main.py
+++ item65C_main.py
@@ -1,19 +1,28 @@
+
 from tkinter import *
 from tkinter import ttk
-import shutil
-import os
-from tkinter import filedialog
-from tkinter import messagebox
+##import shutil
+##import os
+##from tkinter import filedialog
+##from tkinter import messagebox
 
+import item65C_func
 import item65C_gui
-import item65C_func 
+
+
 
 class start(Frame):
     def __init__(self,master, *args, **kwargs):
         Frame.__init__(self, master, *args, **kwargs)
         self.master = master
         master.title("File Transfer")
+        self.master.minsize(300,180) #(Height, Width)
+        self.master.maxsize(300,180)
 
+##        src_files = ''
+##        dest_path = ''
+##        count = 0
+        
         item65C_gui.load_gui(self)
         
 
